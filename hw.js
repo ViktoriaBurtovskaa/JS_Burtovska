85f973da71baae7015b448d29223aa5c82b8e6b8
ViktoriaBurtovskaa <108117423+ViktoriaBurtovskaa@users.noreply.github.com>
2022-09-09 11:04:22 +0200

    Add files via upload

--- hw.js
+++ hw.js
@@ -1,72 +1,50 @@
-// Напишите функцию camelize(str), которая преобразует строки вида «my-short-string» в «myShortString».
-// То есть дефисы удаляются, а все слова после них получают заглавную букву.
+// Создайте input type text и три кнопки
+// кнопки должны иметь следующий функционал (должны запускать соотвествующие фунции)
+// 1. Кнопка "Очистить input" (удалять содержимое value тега input)
+// 2. Кнопка заблокировать инпут (disabled setAttribute)
+// 3. Кнопка разблокировать инпут (disabled removeAttribute)
+// или input.disabled = true/false;
 
-// Примеры:
-// camelize("background-color") == 'backgroundColor';
-// camelize("list-style-image") == 'listStyleImage';
-// camelize("-webkit-transition") == 'WebkitTransition';
+let clearButton = document.querySelector(".clear-button")
+let blockButton = document.querySelector(".block-button")
+let unblockButton = document.querySelector(".unblock-button")
+let input = document.querySelector(".input")
 
+clearButton.addEventListener("click", clearInput)
+blockButton.addEventListener("click", blockInput)
+unblockButton.addEventListener("click", unblockInput)
 
-
-
-function camelize(str) {
-    
-    let strSplit = str.split("-")
-    let strArray = [];
-    for (let i = 0; i < strSplit.length; i++) {
-        const element = strSplit[i];
-        if (element != "") {
-        strArray.push(element);
-        }
-    }
-    let strArrayMapped = strArray.map(function (word,i) {
-        if (i === 0) {
-            return word
-        }else{
-            let firstLetter = word[0].toUpperCase() ;
-            let restLetters = word.slice(1);
-            let changedWord = firstLetter + restLetters;
-            return changedWord;
-        }
-    
-    })
-    return strArrayMapped.join("");
+function clearInput() {
+    input.value = "";
+    console.log("clearInput");
 }
-let str = "webkit-transition"; 
- console.log(camelize(str));
 
-
-
-
-
-
-// Задача: написать функцию, принимающую массив чисел
-//     и возвращающую сумму всех его положительных элементов и отрицательных 
-//     чисел отдельно в виде нового массива
-
-
-//     например функция принимает как аргумент следующий массив
-//     arr = [1, -2, 3, 4, -9]
-
-//     и должна вернуть [8, -11]
-//     Сохраняйте вызов этой функции через деструктурирующее присваивание
-//     Деструктурирующее присвоение означает, что возвращаемый результат функции (return) возвращает 
-//     массив из двух элементов и соответственно его нужно сохранить в переменную и вывести в консоль
-
-let arr = [1, -2, 3, 4, -9]
-
-function sum(arrayOfNumbers) {
-    let newArr = []
-    let sumOfPos = 0
-    let sumOfNeg = 0
-    for (let i = 0; i < arr.length; i++) {
-        let elements = arr[i]
-        if (elements >= 0) {
-            sumOfPos+=elements
-        } else{
-            sumOfNeg+=elements
-        }
-    } 
-return newArr.concat(sumOfPos,sumOfNeg)
+function blockInput() {
+  input.setAttribute("disabled",true);
+  console.log("blockInput");
+//  input.disabled = true;
 }
-console.log(sum(arr));
+function unblockInput() {
+ input.removeAttribute("disabled");
+ console.log("unblockInput");
+//  input.disabled = false;
+
+}
+
+
+// Second level:         Создайте в HTML два текстовых инпута и кнопку
+// Напишите функцию, которая будет по нажатию на кнопку
+// менять содержимое инпутов (value) местами
+
+let firstInput = document.querySelector(".first-input");
+let secondInput = document.querySelector(".second-input");
+let swapButton = document.querySelector(".swap-button");
+
+swapButton.addEventListener("click",swapTwoInputs)
+
+function swapTwoInputs() {
+    let firstInputValue = firstInput.value;
+    let secondInputValue = secondInput.value;
+    firstInput.value = secondInputValue;
+    secondInput.value = firstInputValue;
+}
