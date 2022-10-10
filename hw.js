6c536f7b9e2ed5ccd55495d4c446bb6cad9986dc
ViktoriaBurtovskaa <108117423+ViktoriaBurtovskaa@users.noreply.github.com>
2022-10-10 12:39:16 +0200

    Add files via upload

--- hw.js
+++ hw.js
@@ -1,50 +1,70 @@
-// Создайте input type text и три кнопки
-// кнопки должны иметь следующий функционал (должны запускать соотвествующие фунции)
-// 1. Кнопка "Очистить input" (удалять содержимое value тега input)
-// 2. Кнопка заблокировать инпут (disabled setAttribute)
-// 3. Кнопка разблокировать инпут (disabled removeAttribute)
-// или input.disabled = true/false;
-
-let clearButton = document.querySelector(".clear-button")
-let blockButton = document.querySelector(".block-button")
-let unblockButton = document.querySelector(".unblock-button")
-let input = document.querySelector(".input")
-
-clearButton.addEventListener("click", clearInput)
-blockButton.addEventListener("click", blockInput)
-unblockButton.addEventListener("click", unblockInput)
-
-function clearInput() {
-    input.value = "";
-    console.log("clearInput");
+// ДЗ Функция cut принимает на вход строку, режет её посередине 
+// и возвращает первую половину. Если в строке нечётное число
+// символов, то округлить в меньшую сторону
+function cut1(str) {
+   let res;
+   if (str.length % 2 !== 0) {
+     res = str.slice(0,Math.floor(str.length/2));
+    return res;
+   }
+   res = str.slice(0,str.length/2)
+   return res;
 }
+console.log(cut1("Ура, сегодня пятница!"));
+console.log(cut1("Ура, сегодня пятница"));
+console.log(cut1("Ура, сегодня пятница!!!"));
 
-function blockInput() {
-  input.setAttribute("disabled",true);
-  console.log("blockInput");
-//  input.disabled = true;
-}
-function unblockInput() {
- input.removeAttribute("disabled");
- console.log("unblockInput");
-//  input.disabled = false;
-
+function cut(str) {
+    let strHalfLength;
+    if (str.length % 2 == 0) {
+        strHalfLength = str.length / 2;
+    }else{
+        strHalfLength = Math.floor(str.length / 2);
+    }
+    return str.substring(0,strHalfLength);
 }
 
 
-// Second level:         Создайте в HTML два текстовых инпута и кнопку
-// Напишите функцию, которая будет по нажатию на кнопку
-// менять содержимое инпутов (value) местами
+ // ДЗ Функция kingSaid принимает на вход строку str и добавляет к 
+// её началу фразу "Король сказал: ". Если строка уже начинается
+// с фразы "Король сказал: ", то ничего добавлять не надо.
+// пример: kingSaid("хочу банан") -> "Король сказал: хочу банан"
+//         kingSaid("Король сказал: я устал, я ухожу") -> "Король сказал: я устал, я ухожу"
 
-let firstInput = document.querySelector(".first-input");
-let secondInput = document.querySelector(".second-input");
-let swapButton = document.querySelector(".swap-button");
-
-swapButton.addEventListener("click",swapTwoInputs)
-
-function swapTwoInputs() {
-    let firstInputValue = firstInput.value;
-    let secondInputValue = secondInput.value;
-    firstInput.value = secondInputValue;
-    secondInput.value = firstInputValue;
+function kingSaid(str) {
+    const re = /Король сказал:/g;
+    if (str.match(re)) {
+        return str;
+    }
+    return `Король сказал: ${str}`;
 }
+
+
+// ДЗ Функция isItFridayToday возвращает строку "Пятница будет 
+// через N дней", "Пятница уже завтра!", "Ура, сегодня пятница!" 
+// или "Пятница была вчера :(" в зависимости от текущего дня недели
+
+function isItFridayToday() {
+    const currentDate = new Date();
+    const currentDay = currentDate.getDay();
+    let fridayWillBein;
+    if (currentDay === 5) {
+        return "Ура, сегодня пятница!";
+    }
+    if (currentDay === 6) {
+        return "Пятница была вчера :(";
+    }
+    if (currentDay === 4) {
+        return "Пятница уже завтра!";
+    }
+    if (currentDay === 0) {
+        fridayWillBein = Math.abs(currentDay - 5);
+   return `Пятница будет через ${fridayWillBein} дней`
+    }
+    if (currentDay === 1 || currentDay === 2 || currentDay === 3) {
+        fridayWillBein = Math.abs(currentDay - 5);
+   return `Пятница будет через ${fridayWillBein} дня`
+    }
+}
+
+console.log(isItFridayToday());
