ccf5212f2823fbf5a3db9be450e5e7110d574982
ViktoriaBurtovskaa <108117423+ViktoriaBurtovskaa@users.noreply.github.com>
2022-10-27 23:56:49 +0200

    Add files via upload

--- hw.js
+++ hw.js
@@ -1,70 +1,81 @@
-// ДЗ Функция cut принимает на вход строку, режет её посередине 
-// и возвращает первую половину. Если в строке нечётное число
-// символов, то округлить в меньшую сторону
-function cut1(str) {
-   let res;
-   if (str.length % 2 !== 0) {
-     res = str.slice(0,Math.floor(str.length/2));
-    return res;
-   }
-   res = str.slice(0,str.length/2)
-   return res;
+// ДЗ функция countWordPrice принимает на вход слово word и массив prices вида
+// { letter: <буква>, price: <число> } и возвращает стоимость слова
+function countWordPrice(word, prices) {
+    let res = [];
+    const arrWord = word.split("");
+arrWord.forEach(function (item) {
+    const findLetter = prices.find(function (elem) {
+        return elem.letter === item;
+    });
+    res.push(findLetter.price);
+});
+return res.reduce(function (acc, value) {
+    return acc + value;
+}, 0);
+};
+
+
+console.log(countWordPrice("волк", [
+    { letter: "о", price: 1 },
+    { letter: "л", price: 1 },
+    { letter: "к", price: 2 },
+    { letter: "в", price: 5 },
+])); // 9
+
+
+
+
+// ДЗ функция whoTakeABook принимает на вход массив читателей, массив книг,
+// массив билетов и название книги. Возвращает имя читателя, который взял
+// книгу
+
+function whoTakeABook(readers, books, records, bookName) {
+    const book = books.find((item) => item.bookName === bookName);
+    if (book) {
+      const currentRecord = records.find((item) => item.book === book.id);
+      return currentRecord.reader;
+    }
+    return null;
+  };
+
+
+console.log(whoTakeABook(
+    [ "Петя", "Вася", "Коля" ],
+    [ 
+        { id: 1, bookName: "Сказка о золотой рыбке" },
+        { id: 2, bookName: "Чёрный обелиск" },
+        { id: 3, bookName: "Норвежский лес" }
+    ],
+    [ 
+        { reader: "Петя", book: 2 }, // Петя взял "Чёрный обелиск"
+        { reader: "Петя", book: 1 }, // Петя взял "Сказка о золотой рыбке"
+        { reader: "Коля", book: 3 }, // Коля взял "Норвежский лес"
+    ],
+    "Чёрный обелиск"
+)); // Петя
+
+
+// ДЗ* Функция solveEquasion принимает на вход строку вида
+// x <знак операции> <число> = <число> и возвращает значение x
+function solveEquasion(str) {
+const arr = str.split(" ");//"x","+","5","=","9"
+const a = +arr[arr.length - 1]; //9
+const b = +arr[2] // 5
+const operation = arr[1]; // +
+if (operation === "+") {
+    return `x = ${a - b}`
 }
-console.log(cut1("Ура, сегодня пятница!"));
-console.log(cut1("Ура, сегодня пятница"));
-console.log(cut1("Ура, сегодня пятница!!!"));
-
-function cut(str) {
-    let strHalfLength;
-    if (str.length % 2 == 0) {
-        strHalfLength = str.length / 2;
-    }else{
-        strHalfLength = Math.floor(str.length / 2);
-    }
-    return str.substring(0,strHalfLength);
+if (operation === "-") {
+    return `x = ${a + b}`
 }
-
-
- // ДЗ Функция kingSaid принимает на вход строку str и добавляет к 
-// её началу фразу "Король сказал: ". Если строка уже начинается
-// с фразы "Король сказал: ", то ничего добавлять не надо.
-// пример: kingSaid("хочу банан") -> "Король сказал: хочу банан"
-//         kingSaid("Король сказал: я устал, я ухожу") -> "Король сказал: я устал, я ухожу"
-
-function kingSaid(str) {
-    const re = /Король сказал:/g;
-    if (str.match(re)) {
-        return str;
-    }
-    return `Король сказал: ${str}`;
+if (operation === "*") {
+    return `x = ${a / b}`
 }
-
-
-// ДЗ Функция isItFridayToday возвращает строку "Пятница будет 
-// через N дней", "Пятница уже завтра!", "Ура, сегодня пятница!" 
-// или "Пятница была вчера :(" в зависимости от текущего дня недели
-
-function isItFridayToday() {
-    const currentDate = new Date();
-    const currentDay = currentDate.getDay();
-    let fridayWillBein;
-    if (currentDay === 5) {
-        return "Ура, сегодня пятница!";
-    }
-    if (currentDay === 6) {
-        return "Пятница была вчера :(";
-    }
-    if (currentDay === 4) {
-        return "Пятница уже завтра!";
-    }
-    if (currentDay === 0) {
-        fridayWillBein = Math.abs(currentDay - 5);
-   return `Пятница будет через ${fridayWillBein} дней`
-    }
-    if (currentDay === 1 || currentDay === 2 || currentDay === 3) {
-        fridayWillBein = Math.abs(currentDay - 5);
-   return `Пятница будет через ${fridayWillBein} дня`
-    }
+if (operation === "/") {
+    return `x = ${a * b}`
 }
+return 0;
+};
 
-console.log(isItFridayToday());
+console.log(solveEquasion("x + 5 = 9")); // 4
+console.log(solveEquasion("x * 5 = 30")); // 6
